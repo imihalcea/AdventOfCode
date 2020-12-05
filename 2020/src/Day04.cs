f92a6c1f5a07549c69cdda04e14b3e4435c2ddd9
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2020-12-05 14:03:30 +0100

    2020 day 4 remove duplication of the mandatory fields list

--- 2020/src/Day04.cs
+++ 2020/src/Day04.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 using static System.Environment;
 using static System.String;
@@ -25,44 +24,16 @@ namespace _2020
 
     public class PassportInfo
     {
-        private readonly IDictionary<string, string> _fields;
-        private readonly string[] _mandatory;
-
-        public PassportInfo(string passportData)
-        {
-            _fields = passportData.Trim()
-                        .Split(' ')
-                        .Select(info => info.Split(':'))
-                        .ToDictionary(info => info[0], info => info[1]);
-            
-            _mandatory = new[] {"byr","iyr","eyr","hgt","hcl","ecl","pid"};
-        }
-
         public bool IsComplete() => 
-            _fields.Keys.Intersect(_mandatory).Count() == _mandatory.Length;
+            _fields.Keys.Intersect(Mandatory).Count() == Mandatory.Length;
 
-        private bool AllFieldsValid() =>
-            _fields.All(kv => Validators(kv.Key)(kv.Value));
+        private bool AllValid() =>
+            _fields.All(kv => FieldValidators.ContainsKey(kv.Key) && FieldValidators[kv.Key](kv.Value));
 
-        public bool IsValid() => IsComplete() && AllFieldsValid();
-
-        private static Func<string, bool> Validators(string field) =>
-            field switch
-            {
-                "byr" => NumberValidator(1920, 2002),
-                "iyr" => NumberValidator(2010, 2020),
-                "eyr" => NumberValidator(2020, 2030),
-                "hgt" => HeightValidator,
-                "hcl" => PatternValidator(@"^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$"),
-                "ecl" => AnyValidator(new[] {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}),
-                "pid" => PatternValidator("^[0-9]{9}$"),
-                "cid" => _ => true,
-                _ => __ => false
-            };
+        public bool IsValid() => IsComplete() && AllValid();
 
         private static Func<string, bool> NumberValidator(int min, int max) =>
-            data => int.TryParse(data, out var byr) 
-                    && byr >= min && byr <= max;
+            data => int.TryParse(data, out var n) && n >= min && n <= max;
 
         private static bool HeightValidator(string data)
         {
@@ -78,7 +49,28 @@ namespace _2020
         private static Func<string, bool> PatternValidator(string pattern) =>
             data => new Regex(pattern).IsMatch(data);
 
-        private static Func<string, bool> AnyValidator(string[] set) => 
-            set.Contains;
+        public PassportInfo(string passportData)
+        {
+            _fields = passportData.Trim()
+                .Split(' ')
+                .Select(info => info.Split(':'))
+                .ToDictionary(info => info[0], info => info[1]);
+        }
+        
+        private readonly IDictionary<string, string> _fields;
+
+        private readonly string[] Mandatory = FieldValidators.Keys.Take(7).ToArray();
+
+        private static readonly Dictionary<string,Func<string,bool>> FieldValidators = new Dictionary<string, Func<string, bool>>()
+        {
+            {"byr", NumberValidator(1920, 2002)},
+            {"iyr", NumberValidator(2010, 2020)},
+            {"eyr", NumberValidator(2020, 2030)},
+            {"hgt", HeightValidator},
+            {"hcl", PatternValidator(@"^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$")},
+            {"ecl", PatternValidator("^(amb|blu|brn|gry|grn|hzl|oth)$")},
+            {"pid", PatternValidator("^[0-9]{9}$")},
+            {"cid", _=>true}
+        };
     }
 }
