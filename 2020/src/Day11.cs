2c8227943f0d589b69b07a5dd9b108e64828da49
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2020-12-13 22:33:57 +0100

    Day11 - part 1 & 2

--- 2020/src/Day11.cs
+++ 2020/src/Day11.cs
@@ -1,54 +1,155 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static _2020.State;
 
 namespace _2020
 {
     public class Day11
     {
+        public static int Part2(Seats seats)
+        {
+            var hasEvolved = true;
+            while (hasEvolved) 
+                hasEvolved = seats.Evolve2(5);
+            return seats.CountOccupied();
+        }
+        public static int Part1(Seats seats)
+        {
+            var hasEvolved = true;
+            while (hasEvolved) 
+                hasEvolved = seats.Evolve1(4);
+            return seats.CountOccupied();
+        }
         public static Seats Dataset(string path)
         {
-            
+            var lines = File.ReadAllLines(path);
+            var seats = new Seats(lines[0].Length-1, lines.Length-1);
+            for (var y = 0; y < lines.Length; y++)
+            {
+                var line = lines[y].ToCharArray();
+                for (var x = 0; x < line.Length; x++)
+                {
+                    seats.AddSeat(line[x],x,y);
+                }
+            }
+            return seats;
         } 
     }
 
     public class Seats
     {
-        private List<Seat> _seats;
-
-        public Seats()
+        public int Xm { get; }
+        public int Ym { get; }
+        
+        private List<State> _states;
+        private readonly int[][] _coordsToIndex;
+        private readonly (int, int)[] _indexToCoords;
+        public Seats(int xm, int ym)
         {
-            _seats = new List<Seat>();
-        }
-
-
-        public void AddSeat(char c, int x, int y)
-        {
-            if (c == 'L')
-                _seats.Add(new Seat(x,y, State.Empty));
-        }
-            
-    }
-    
-    
-    public class Seat
-    {
-        public int X { get; }
-        public int Y { get; }
-        public State SeatState { get; }
-
-        public Seat(int x, int y, State seatState)
-        {
-            X = x; Y = y; SeatState = seatState;
-        }
-
-        public Seat Evolve(int occupiedAdjSeats) =>
-            occupiedAdjSeats switch
+            Xm = xm;
+            Ym = ym;
+            _states = new List<State>();
+            _coordsToIndex = new int[Xm+1][];
+            for (int x = 0; x <= xm; x++)
             {
-                {} when occupiedAdjSeats == 0 && IsEmpty => new Seat(X,Y,State.Occupied),
-                {} when occupiedAdjSeats >= 4 && IsOccupied => new Seat(X,Y,State.Empty),
-                _ => this
+                _coordsToIndex[x] = new int[ym+1];
+            }
+            _indexToCoords = new (int, int)[(Ym+1)*(Xm+1)];
+        }
+
+        public Seats AddSeat(char c, int x, int y)
+        {
+            _states.Add(c == 'L' ? Empty : Floor);
+            var index = _states.Count - 1;
+            _coordsToIndex[x][y] = index;
+            _indexToCoords[index] = (x, y);
+            return this;
+        }
+
+        private State GetState((int x, int y) p)
+        {
+            try
+            {
+                var seat = _states[CoordsToIndex(p)];
+                return seat;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        private int CoordsToIndex((int x, int y) p) => _coordsToIndex[p.x][p.y];
+
+        private (int x, int y) IndexToCoords(int index) => _indexToCoords[index];
+        
+
+        public Func<int, bool> Evolve1 => limit => Evolve(limit, OccupiedAdj);
+        public Func<int, bool> Evolve2 => limit => Evolve(limit, OccupiedSeen);
+
+        public bool Evolve(int limit, Func<int, int> f)
+        {
+            var nextStates = _states
+                .Select((state,index)=>(state,index))
+                .Select(item => EvolveState(item.state,f(item.index), limit))
+                .ToList();
+            var hasEvolved = !_states.SequenceEqual(nextStates);
+            _states = nextStates;
+            return hasEvolved;
+        }
+
+        private State EvolveState(State currentState, int occupied, int limit) =>
+            (currentState, occupied) switch
+            {
+                var (cs, o) when (cs, o) == (Empty, 0) => Occupied,
+                var (cs, o) when cs == Occupied && o >= limit => Empty,
+                _ => currentState
             };
-        private bool IsEmpty => SeatState == State.Empty;
-        private bool IsOccupied => SeatState == State.Occupied;
+
+
+        public int CountOccupied() => _states.Count(s => s==Occupied);
+        
+        private int OccupiedAdj(int index) => 
+            Adj(IndexToCoords(index)).Where(IsValid).Count(point => GetState(point) == Occupied);
+
+
+        private int OccupiedSeen(int index) => 
+            Directions.Sum(d => Look(d, IndexToCoords(index)));
+
+        private int Look(Func<(int, int), (int, int)> direction, (int x, int y) p)
+        {
+            while (true)
+            {
+                p = direction(p);
+                if (!IsValid(p)) return 0;
+                var state = GetState(p);
+                if (state != Floor )
+                    return state==Occupied?1:0;
+            }
+        }
+
+        private bool IsValid((int x, int y) p) => (p.x >= 0 && p.x <= Xm) && (p.y >= 0 && p.y <= Ym);
+        private static Func<(int, int), (int, int)>[] Directions =>
+            new Func<(int, int), (int, int)>[]
+            {
+               p => (p.Item1- 1, p.Item2),
+               p => (p.Item1 + 1, p.Item2),
+               p => (p.Item1, p.Item2 - 1),
+               p => (p.Item1, p.Item2 + 1),
+               p => (p.Item1 - 1, p.Item2 - 1),
+               p => (p.Item1 + 1, p.Item2 - 1),
+               p => (p.Item1 + 1, p.Item2 + 1),
+               p => (p.Item1 - 1, p.Item2 + 1),
+            };
+
+        private static (int, int)[] Adj((int x, int y) p) =>
+            Directions.Select(d => d(p)).ToArray();
     }
-    public enum State { Occupied, Empty }
+    
+
+    public enum State {
+        Occupied, Empty, Floor }
 }
