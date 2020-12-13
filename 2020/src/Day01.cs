c313b2b9aca14dfb3725d529e87f0bcccf07447f
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2020-12-13 22:34:33 +0100

    Day1 - much simpler alternative 1 & 2

--- 2020/src/Day01.cs
+++ 2020/src/Day01.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
@@ -8,7 +9,30 @@ namespace _2020
 {
     public static class Day01
     {
+
         public static int Part1(string filePath)
+        {
+            var input = DataSet(filePath);
+            return (from a in input
+                    let b = 2020 - a
+                    where input.Contains(b)
+                    select a * b)
+                .First();
+        }
+        
+        public static int Part2(string filePath)
+        {
+            var input = DataSet(filePath);
+            return (from a in input
+                    from b in input
+                    let c = 2020 - a - b
+                    where input.Contains(c)
+                    select a * b *c)
+                .First();
+        }
+
+
+        public static int Part1_comb(string filePath)
         {
             var m = DataSet(filePath)
                 .Combinations(2)
@@ -16,7 +40,7 @@ namespace _2020
             return m[0] * m[1];
         }
 
-        public static int Part2(string filePath)
+        public static int Part2_comb(string filePath)
         {
             var m = DataSet(filePath)
                     .Combinations(3)
@@ -24,8 +48,8 @@ namespace _2020
             return m[0] * m[1] * m[2];
         }
         
-        private static int[] DataSet(string filePath) =>
-            File.ReadAllLines(filePath).Select(int.Parse).ToArray();
+        private static HashSet<int> DataSet(string filePath) =>
+            File.ReadAllLines(filePath).Select(int.Parse).ToHashSet();
 
 
   
