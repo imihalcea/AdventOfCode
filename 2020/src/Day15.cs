6ccfcd4fafca1311535f01c59ed04c6eb76705f2
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2020-12-16 22:05:17 +0100

    Day15 - part1 and 2

--- 2020/src/Day15.cs
+++ 2020/src/Day15.cs
@@ -1,40 +1,47 @@
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics;
 
 namespace _2020
 {
     public class Day15
     {
-        public static int Part1(int[] dataset)
+        public static int Part1((int[], int) input)
         {
-            var spoken = new int[2021];
-            spoken[0] = 0;
-            var mem = new SortedDictionary<int,List<int>>();
-            dataset.CopyTo(spoken,0);
-            
-            for (var turn = dataset.Length; turn <= 2020; turn++)
+            var (dataset, n) = input;
+            var mem = new ConcurrentDictionary<int,int?[]>();
+            for (var i = 0; i < dataset.Length; i++)
             {
-                var lastSpoken = spoken[turn -1];
-                var previousTurnsOfLastSpoken = mem.GetValueOrDefault(lastSpoken, new List<int>());
-                mem[lastSpoken] = previousTurnsOfLastSpoken;
-                if (previousTurnsOfLastSpoken.Count==0)
-                {
-                    spoken[turn] = 0;
-                }
-                else
-                {
-                    spoken[turn] = previousTurnsOfLastSpoken.Last() - previousTurnsOfLastSpoken.First();
-                }
-
-                var cs = mem.GetValueOrDefault(spoken[turn], new List<int>());
-                cs.Add(turn);
-                mem[spoken[turn]] = cs;
+                mem[dataset[i]] = new int?[]{i+1,null};
             }
-
-            return spoken[2020];
+            var lastSpoken = dataset.Last();            
+            for (var turn = dataset.Length+1; turn <= n; turn++)
+            {
+                var pastOccurrences = mem[lastSpoken];
+                int next = pastOccurrences[1]==null ? 0 : pastOccurrences[1]!.Value - pastOccurrences[0]!.Value;
+                var currentTurn = turn;
+                mem.AddOrUpdate(
+                    key:next,
+                    addValueFactory: _ => new int?[] {currentTurn, null},
+                    updateValueFactory: (k, v) =>
+                    {
+                        Swap(ref v[0], ref v[1]);
+                        v[1] = currentTurn;
+                        return v;
+                    });
+                lastSpoken = next;
+            }
+            return lastSpoken;
         }
-        
-        
+
+        static void Swap(ref int? a, ref int? b)
+        {
+            if(!b.HasValue) return;
+            int? temp = a;
+            a = b;
+            b = temp;
+        }
         public static int[] Dataset(string input) => input.Split(",").Select(int.Parse).ToArray();
     }
 }
