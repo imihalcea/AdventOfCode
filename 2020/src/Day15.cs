37f603f00180e721b5e31834022818287b0edb7b
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2020-12-16 22:48:44 +0100

    Day15 - part1 and 2

--- 2020/src/Day15.cs
+++ 2020/src/Day15.cs
@@ -10,38 +10,24 @@ namespace _2020
         public static int Part1((int[], int) input)
         {
             var (dataset, n) = input;
-            var mem = new ConcurrentDictionary<int,int?[]>();
-            for (var i = 0; i < dataset.Length; i++)
-            {
-                mem[dataset[i]] = new int?[]{i+1,null};
-            }
-            var lastSpoken = dataset.Last();            
-            for (var turn = dataset.Length+1; turn <= n; turn++)
-            {
-                var pastOccurrences = mem[lastSpoken];
-                int next = pastOccurrences[1]==null ? 0 : pastOccurrences[1]!.Value - pastOccurrences[0]!.Value;
-                var currentTurn = turn;
-                mem.AddOrUpdate(
-                    key:next,
-                    addValueFactory: _ => new int?[] {currentTurn, null},
-                    updateValueFactory: (k, v) =>
-                    {
-                        Swap(ref v[0], ref v[1]);
-                        v[1] = currentTurn;
-                        return v;
-                    });
-                lastSpoken = next;
-            }
-            return lastSpoken;
+            return Generator(dataset, n);
         }
 
-        static void Swap(ref int? a, ref int? b)
+        private static int Generator(int[] input, int limit)
         {
-            if(!b.HasValue) return;
-            int? temp = a;
-            a = b;
-            b = temp;
+            var mem = input.Select((value,idx)=>(value,idx)).ToDictionary(it=>it.value, it=>it.idx+1);
+            var last = input[^1];
+            var turn = mem.Count;
+            while (turn<limit)
+            {
+                var next = mem.ContainsKey(last) ? turn - mem[last] : 0;
+                mem[last] = turn;
+                last = next;
+                turn++;
+            }
+            return last;
         }
+        
         public static int[] Dataset(string input) => input.Split(",").Select(int.Parse).ToArray();
     }
 }
