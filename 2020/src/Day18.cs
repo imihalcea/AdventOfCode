75fac8eda06d8821d3cb99629ac99fc66ac7a8bc
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2020-12-20 23:24:41 +0100

    day 18 - needs heavy refactoring

---
+++ 2020/src/Day18.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _2020
+{
+    
+    
+    public class Day18
+    {
+
+
+        public static long Part1(string[] dataset) => 
+            dataset
+                .Select(Parse)
+                .Sum(op=>op.Eval());
+        
+        public static long Part2(string[] dataset) => 
+            dataset
+                .Select(Parse2)
+                .Sum(op=>op.Eval());
+
+
+        public static string[] Dataset(string filePath) => 
+            File.ReadAllLines(filePath);
+
+        public static Op Parse(string expr)
+        {
+            var regExp = new Regex(@"\(([^()]+)\)");
+            while (expr.Contains('('))
+            {
+                expr = regExp.Replace(expr, m => ParseBasicOp(m.Value.Replace("(","").Replace(")","")).Eval().ToString());
+            }
+            return ParseBasicOp(expr);
+        }
+
+        public static Op Parse2(string expr)
+        {
+            var regExp = new Regex(@"\(([^()]+)\)");
+            while (expr.Contains('('))
+            {
+                expr = regExp.Replace(expr, m => ParseBasicOp2(m.Value.Replace("(","").Replace(")","")).Eval().ToString());
+            }
+            return ParseBasicOp2(expr);
+        }
+
+        
+        private static Op ParseBasicOp2(string expr)
+        {
+            var parts = expr.Split(' ');
+            var opSymbols = parts.Where(p => p=="+" || p=="*").ToList();
+            var ops = parts.Where(p => p!="+" && p!="*").Select(d=> new Id(long.Parse(d))).Cast<Op>().ToList();
+            
+            while (opSymbols.Contains("+"))
+            {
+                (ops, opSymbols) = ParsePrioritary(ops, opSymbols);
+            }
+
+            var basicOp = opSymbols.Zip(ops.Skip(1))
+                .Aggregate(
+                    ops.First(), 
+                    (acc, n) => CreateOp(n.First, acc, n.Second)
+                );
+            return basicOp;
+        }
+
+        public static (List<Op> ops, List<string> symbols) ParsePrioritary(List<Op> ops, List<string> symbols)
+        {
+            var index = symbols.FindIndex(s => s == "+");
+            var opSym = symbols[index];
+            var left = ops[index];
+            var right = ops[index + 1];
+            var op = CreateOp(opSym, left, right);
+            ops.Remove(left);
+            ops.Remove(right);
+            ops.Insert(index, op);
+            symbols.RemoveAt(index);
+            return (ops, symbols);
+        }
+
+        private static Op CreateOp(string symbol, Op left, Op right) =>
+            symbol switch
+            {
+                "*" => new Mul(left, right),
+                "+" => new Add(left, right),
+                _ => throw new NotSupportedException()
+            };
+
+        private static Op ParseBasicOp(string expr)
+        {
+            
+            var parts = expr.Split(' ');
+            var opSymbols = parts.Where(p => p=="+" || p=="*").ToArray();
+            var nums = parts.Where(p => p!="+" && p!="*").Select(d=> new Id(long.Parse(d))).ToArray();
+
+            var basicOp = opSymbols[1..].Zip(nums[2..])
+                .Aggregate(
+                    CreateOp(opSymbols[0], nums[0], nums[1]), 
+                    (acc, n) => CreateOp(n.First, acc, n.Second)
+                );
+            return basicOp;
+        }
+    }
+
+    
+    
+    public interface Op
+    {
+        long Eval();
+    }
+
+    public class Mul : Op
+    {
+        public Op Left { get; }
+        public Op Right { get; }
+
+        public Mul(Op left, Op right)
+        {
+            Left = left;
+            Right = right;
+        }
+        public long Eval() => Left.Eval() * Right.Eval();
+    }
+
+    public class Add : Op
+    {
+        public Op Left { get; }
+        public Op Right { get; }
+
+        public Add(Op left, Op right)
+        {
+            Left = left;
+            Right = right;
+        }
+        public long Eval() => Left.Eval() + Right.Eval();
+    }
+
+    public class Id : Op
+    {
+        private readonly long _a;
+
+        public Id(long a)
+        {
+            _a = a;
+        }
+
+        public long Eval() => _a;
+    }
+}
---
+++ 2020/test/Day18Test.cs
@@ -0,0 +1,66 @@
+using NFluent;
+using NUnit.Framework;
+using static _2020.Day18;
+using static _2020.test.TestExtensions;
+
+namespace _2020.test
+{
+    [TestFixture]
+    public class Day18Test
+    {
+        private const string INPUT_FILE_PATH = "test/day18.txt";
+
+        [Test]
+        public void answer_part1()
+        {
+            var n = Answer(Part1,  Dataset(INPUT_FILE_PATH));
+            Check.That(n).IsEqualTo(67800526776934);
+        }
+        
+        [Test]
+        public void answer_part2()
+        {
+            var n = Answer(Part2,  Dataset(INPUT_FILE_PATH));
+            Check.That(n).IsEqualTo(340789638435483);
+        }
+        
+        [Test]
+        public void simple_expression()
+        {
+            //1 + 2 * 3 + 4 * 5 + 6
+            var op1 = new Add(new Mul(new Add(new Mul(new Add(new Id(1), new Id(2)),new Id(3)),new Id(4)),new Id(5)),new Id(6));
+            Check.That(op1.Eval()).IsEqualTo(71);
+            //1 + (2 * 3) + (4 * (5 + 6))
+            var op2 = new Add(new Add(new Id(1), new Mul(new Id(2), new Id(3))), new Mul(new Id(4), new Add(new Id(5), new Id(6))));
+            Check.That(op2.Eval()).IsEqualTo(51);
+
+        }
+
+        [TestCase("1 + 1",2)]
+        [TestCase("1 + 1 * 3",6)]
+        [TestCase("1 + (2 * 3) + (4 * (5 + 6))",51)]
+        [TestCase("2 * 3 + (4 * 5)",26)]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)",437)]
+        [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",12240)]
+        [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2",13632)]
+        public void parse_and_eval_part1(string expr, int expected)
+        {
+            var op = Parse(expr);
+            Check.That(op.Eval()).IsEqualTo(expected);
+        }
+
+        [TestCase("1 + 1",2)]
+        [TestCase("1 + 1 * 3",6)]
+        [TestCase("1 + (2 * 3) + (4 * (5 + 6))",51)]
+        [TestCase("2 * 3 + (4 * 5)",46)]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)",1445)]
+        [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",669060)]
+        [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2",23340)]
+        public void parse_and_eval_part2(string expr, int expected)
+        {
+            var op = Parse2(expr);
+            Check.That(op.Eval()).IsEqualTo(expected);
+        }
+        
+    }
+}
