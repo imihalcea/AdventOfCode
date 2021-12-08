40aaf0476febece6beae75df33067b57f6953dc0
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2021-12-08 01:21:08 +0100

    renaming

--- 2021/Day07/Solution.fs
+++ 2021/Day07/Solution.fs
@@ -1,19 +1,17 @@
 module _2021.Day07.Solution
 
-let fuelForPosition fc pos crabs =
+let fuelForPosition cost pos crabs =
     crabs
-    |> Array.fold (fun acc crab -> fc (abs (crab-pos)) + acc ) 0
+    |> Array.fold (fun acc crab -> cost (abs (crab-pos)) + acc ) 0
 
-let minimumFuel costFun crabs =
+let minimumFuel cost crabs =
     crabs |> Array.sortInPlace    
     {crabs.[0]..crabs.[^1]}
-    |> Seq.map (fun pos -> (fuelForPosition costFun pos crabs))
+    |> Seq.map (fun pos -> (fuelForPosition cost pos crabs))
     |> Seq.min    
 
 let part1 crabs =
     crabs |> minimumFuel id
 
 let part2 crabs =
-    let costFun distance =
-        distance * (distance+1) / 2
-    crabs |> minimumFuel costFun
+    crabs |> minimumFuel (fun d -> d * (d + 1)/2)
