ba34f60cea2c0d73b59e7b6fb0dbc6c441fa3884
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2021-12-08 01:17:47 +0100

    cleanup Day7

--- 2021/Day07/Solution.fs
+++ 2021/Day07/Solution.fs
@@ -2,13 +2,11 @@ module _2021.Day07.Solution
 
 let fuelForPosition fc pos crabs =
     crabs
-    |> Array.map (fun crab -> abs (crab-pos) |> fc)
-    |> Array.sum
+    |> Array.fold (fun acc crab -> fc (abs (crab-pos)) + acc ) 0
 
 let minimumFuel costFun crabs =
-    let posMin = Array.min crabs
-    let posMax = Array.max crabs
-    {posMin..posMax}
+    crabs |> Array.sortInPlace    
+    {crabs.[0]..crabs.[^1]}
     |> Seq.map (fun pos -> (fuelForPosition costFun pos crabs))
     |> Seq.min    
 
