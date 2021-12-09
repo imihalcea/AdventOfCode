bafd17ec5bf36164e4d6aa8f29c7ad5a154bf421
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2021-12-09 21:41:29 +0100

    Day9 recursive version basin size

--- 2021/Day09/Solution.fs
+++ 2021/Day09/Solution.fs
@@ -2,8 +2,7 @@ module _2021.Day09.Solution
 
 open System.Collections.Generic
 
-let mapSize (m:int[,])=
-    (m.[*,0].Length - 1, m.[0,*].Length - 1)
+let mapSize (m:int[,])= (m.[*,0].Length - 1, m.[0,*].Length - 1)
 
 let points rows cols =
     seq{
@@ -17,45 +16,34 @@ let adjacent point rows cols =
     [(r-1,c); (r+1,c); (r, c-1); (r, c+1)]
     |> Seq.filter (fun p -> (fst p) >= 0 && (fst p) <= rows && (snd p) >= 0 && (snd p) <= cols)
 
-let lowPoints (heightMap:int[,]) =
-    let (rows,cols) = mapSize heightMap
+let lowPoints (map:int[,]) =
+    let (rows,cols) = mapSize map
     points rows cols
     |> Seq.filter (fun point ->
-                                (point,rows,cols)
-                                |||> adjacent 
-                                |> Seq.forall (fun adj -> heightMap.[fst point, snd point] < heightMap.[fst adj, snd adj])
+                                (point,rows,cols)|||> adjacent 
+                                |> Seq.forall (fun adj -> map.[fst point, snd point] < map.[fst adj, snd adj])
                    )
 
-let basinsSize (heightMap:int[,]):seq<int> =
-    let (rows,cols) = mapSize heightMap 
+let basinsSizes (map:int[,]):seq<int> =
+    let (rows,cols) = mapSize map 
+    let visited = HashSet()
     
-    let rec basinSize (lowPoint:int * int) =
-        let visited = HashSet()
-        let queue = Queue<int * int>()
+    let rec basinSize (cnt:int) (lowPoint:int * int) =
         visited.Add lowPoint |> ignore
-        queue.Enqueue lowPoint
-        let mutable cnt = 0
-        while queue.Count > 0 do
-            cnt <- cnt + 1
-            let qit = queue.Dequeue() 
-            let adjCells = (qit,rows,cols) |||> adjacent |> Seq.filter (fun cell -> not(heightMap.[fst cell, snd cell] = 9)) |> Seq.toArray
-            
-            for a in adjCells do
-                if not(visited.Contains(a)) then
-                    queue.Enqueue a
-                    visited.Add a |> ignore
-        cnt
+        (lowPoint,rows,cols)|||> adjacent
+        |> Seq.filter (fun p -> not(visited.Contains p) && map.[fst p, snd p] <> 9)
+        |> Seq.fold (fun cnt -> basinSize (cnt+1)) cnt
     
-    heightMap |> lowPoints |> Seq.map basinSize
+    map |> lowPoints |> Seq.map (basinSize 1)
  
-let part1 (heightMap:int[,]) =
-    heightMap |> lowPoints 
-    |> Seq.map (fun c -> 1+heightMap.[fst c, snd c])
+let part1 (map:int[,]) =
+    map |> lowPoints 
+    |> Seq.map (fun c -> 1+map.[fst c, snd c])
     |> Seq.sum
 
-let part2 (heightMap:int[,]) =
-    heightMap
-    |> basinsSize
+let part2 (map:int[,]) =
+    map
+    |> basinsSizes
     |> Seq.sortDescending
     |> Seq.take 3
     |> Seq.fold (fun acc it -> acc * it) 1
