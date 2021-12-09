137a8dd0824cd79755ea55bc32d70a741f49e2d8
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2021-12-09 13:26:37 +0100

    Day9 part2 needs heavy refactor

--- 2021/Day09/Solution.fs
+++ 2021/Day09/Solution.fs
@@ -1,5 +1,7 @@
 module _2021.Day09.Solution
 
+open System.Collections.Generic
+
 let cells rows cols =
     seq{
         for r in 0..rows do
@@ -23,11 +25,36 @@ let adj cell rows cols =
 let isMin (cell:(int * int)) (m:int[,]) (adjs:seq<(int * int)>)=
     let v = m.[fst cell, snd cell]
     adjs
-    |> Seq.map (fun adj ->
-        System.Diagnostics.Trace.WriteLine $"%i{fst adj}, %i{snd adj}"
-        m.[fst adj, snd adj])
+    |> Seq.map (fun adj -> m.[fst adj, snd adj])
     |> Seq.forall (fun adjVal -> v < adjVal)
+
+
+let basinsSize (matrix:int[,]):seq<int> =
+    let rows = matrix.[*,0].Length - 1
+    let cols = matrix.[0,*].Length - 1
     
+    let size (cell:int * int) (matrix:int[,]) =
+        let visited = HashSet()
+        let queue = Queue<(int * int)>()
+        visited.Add cell |> ignore
+        queue.Enqueue cell
+        let mutable cnt = 0
+        while queue.Count > 0 do
+            cnt <- cnt + 1
+            let qit = queue.Dequeue() 
+            let adjCells = (qit,rows,cols) |||> adj |> Seq.filter (fun cell -> not(matrix.[fst cell, snd cell] = 9)) |> Seq.toArray
+            
+            for a in adjCells do
+                if not(visited.Contains(a)) then
+                    queue.Enqueue a
+                    visited.Add a |> ignore
+        cnt
+    
+    cells rows cols
+    |> Seq.filter (fun cell -> (adj cell rows cols) |> isMin cell matrix)
+    |> Seq.filter (fun cell -> not(matrix.[fst cell, snd cell] = 9))
+    |> Seq.map (fun cell -> size cell matrix)
+    |> Seq.sortDescending
 
 let part1 (matrix:int[,]) =
     let rows = matrix.[*,0].Length - 1
@@ -37,3 +64,9 @@ let part1 (matrix:int[,]) =
     |> Seq.filter (fun cell -> (adj cell rows cols) |> isMin cell matrix)
     |> Seq.map (fun c -> 1+matrix.[fst c, snd c])
     |> Seq.sum
+
+let part2 (matrix:int[,]) =
+    matrix
+    |> basinsSize
+    |> Seq.take 3
+    |> Seq.fold (fun acc it -> acc * it) 1
--- 2021/Day09/Tests.fs
+++ 2021/Day09/Tests.fs
@@ -13,28 +13,27 @@ let parseInput filePath =
     |> array2D
 
 [<Fact>]
-let ``adj tests``()=
-    let r = Solution.adj (0,0) 2 2
-    test <@ r |> Seq.toList = [(1,0); (0,1)] @>
-
-[<Fact>]
-let ``adj tests 2``()=    
-    let r1 = Solution.adj (1,1) 2 2 |> Seq.toList
-    test <@ r1 = [(0,1); (2,1); (1,0); (1,2)] @>
-
-[<Fact>]
-let ``adj tests 3``()=    
-    let r1 = Solution.adj (2,2) 2 2 |> Seq.toList
-    test <@ r1 = [(1,2); (2,1)] @>
-
-[<Fact>]
 let ``example part1``()=
    let result = parseInput "Day09/example.txt"
                 |> Solution.part1 
    test <@ result = 15  @>
 
 [<Fact>]
+let ``example part2``()=
+   let result = parseInput "Day09/example.txt"
+                |> Solution.part2 
+   test <@ result = 1134  @>
+
+
+
+[<Fact>]
 let ``part1``()=
    let result = parseInput "Day09/input.txt"
                 |> Solution.part1 
-   test <@ result = 0  @> 
+   test <@ result = 512  @>
+
+[<Fact>]
+let ``part2``()=
+   let result = parseInput "Day09/input.txt"
+                |> Solution.part2 
+   test <@ result = 1600104  @>  
