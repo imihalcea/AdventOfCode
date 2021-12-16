234f86a56578b56602ad50f941b4923572556b8a
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2021-12-16 11:14:10 +0100

    Day15 need to deep dive into fsharp's Map structure to understand performance implications
    in this impl Map.change takes way to long time

--- 2021/Day15/Solution.fs
+++ 2021/Day15/Solution.fs
@@ -1,24 +1,102 @@
 module _2021.Day15.Solution
 
+open System.Collections.Generic
+open FSharpx.Collections
+
 type Point = (int*int)
 type Risk = int
+type Edge = (Point * Point * Risk)
 type CavernShape = int[,]
 
+and Graph = {
+    adjMap:Map<Point, (Point * int) list>
+}
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Graph =
+    let empty  = { adjMap = Map.empty }
+    
+    let addEdge (g: Graph) (edge:Edge) : Graph =
+        let from,``to``, weight = edge
+        let newAdj = Map.change from (fun adj ->
+                            match adj with
+                            | None -> Some([ (``to``, weight) ])
+                            | Some (l) -> Some((``to``, weight) :: l)) g.adjMap
+                        
+        {g with adjMap = newAdj}
+
+    let neighborsOf (v:Point) (g:Graph): (Point * int) list =
+        g.adjMap.[v]
+
+    let djikstra (from: Point) (``to``: Point) (graph: Graph) =
+        let mutable weights = [(from, 0)] |> Map.ofList
+        let mutable current = from
+        let mutable queue = PriorityQueue<Point, int>()
+        queue.Enqueue (from,0)
+        
+        while current <> ``to`` do
+            current <- queue.Dequeue()
+            
+            for (n,w) in neighborsOf current graph do
+                    let acc = weights[current] + w
+                    if acc < weights.GetValueOrDefault(n, 1_000_000) then
+                        queue.Enqueue (n, acc)
+                        weights <- Map.change n (fun _ -> Some(acc)) weights
+        weights  
 
 let mapSize (m:int[,])= (m.[*,0].Length - 1, m.[0,*].Length - 1)
 
-let readMap (p:Point) (m:CavernShape):int = m[fst p, snd p]
+let readMap (m:CavernShape) (p:Point):int = 
+    m[fst p, snd p]
 
-
-let nextPoints (cavern:CavernShape) (from:Point):(Point * int)[]=
-    let h,w = mapSize cavern
+let nextPoints (fRead:Point -> int) (h:int) (w:int) (from:Point):(Point * int)[]=
     let inBounds p =
         (fst p) >= 0 && (fst p) <= h && (snd p) >= 0 && (snd p) <= w
     let r,c = from
-    [|Point (r+1,c);Point (r, c+1)|]
+    [|Point (r+1,c);Point (r, c+1); Point(r-1,c); Point(r, c-1)|]
     |> Array.filter inBounds
-    |> Array.map (fun p -> (p, (readMap p cavern)))
+    |> Array.map (fun p -> (p, fRead p))
     
+let points rows cols =
+          seq{
+            for r in 0..rows do
+               for c in 0..cols do
+                  yield Point(r,c)
+            }
 
-let part1 (debug:string->unit) (map:int[,]) =
-   0
+let computeRisks (topLeft:Point) (bottomRight:Point) (map:CavernShape)=
+   let rows,cols = mapSize map
+   let fread = readMap map
+   let nextPoints = nextPoints fread rows cols
+   let graph = 
+       points rows cols
+       |> Seq.map (fun from -> nextPoints from |> Seq.map (fun ``to`` -> (from, fst ``to``, snd ``to``)))
+       |> Seq.concat
+       |> Seq.cons (bottomRight, bottomRight, 0) //add the bottomRight risk to the graph 
+       |> Seq.fold Graph.addEdge Graph.empty
+   Graph.djikstra topLeft bottomRight graph
+          
+          
+let part1 (map:int[,]) =
+   let rows,cols = mapSize map
+   (computeRisks (Point (0,0)) (Point(rows, cols)) map)[(Point(rows, cols))]
+
+let augment (m:int[,])=
+    let rows, cols = (m.[*,0].Length, m.[0,*].Length)
+    seq{
+        for r in 0..(rows*5)-1 do
+            yield seq{
+                for c in 0..(cols*5)-1 do
+                    let originRow, originColumn = r%rows, c%cols 
+                    let originalValue = m[originRow,originColumn]
+                    let distanceToOrigin = (r/rows) + (c/cols)
+                    let newValue = (originalValue + distanceToOrigin - 1)%9+1
+                    yield newValue
+            }
+    } |> array2D
+    
+    
+let part2 (map:int[,]) =
+   let augmentedMap = augment map
+   let rows,cols = mapSize augmentedMap
+   (computeRisks (Point (0,0)) (Point(rows, cols)) augmentedMap)[(Point(rows, cols))]
--- 2021/Day15/Tests.fs
+++ 2021/Day15/Tests.fs
@@ -4,15 +4,39 @@ open System
 open System.IO
 open Xunit
 open Swensen.Unquote
-open Xunit.Abstractions
+
 let parseInput filePath =
     File.ReadLines filePath
     |> Seq.map (fun l -> (l |> Seq.map (fun c -> Convert.ToInt32(c.ToString()))))
     |> array2D
 
-type MyTests(output:ITestOutputHelper) =
-    [<Fact>]
-    member _.``example part1``()=
+[<Fact>]
+let ``example part1``()=
        let result = parseInput "Day15/example.txt"
-                    |> Solution.part1 output.WriteLine
-       test <@ result = 15  @>
+                    |> Solution.part1
+       test <@ result = 40  @>
+
+[<Fact>]
+let ``part1``()=
+    let result = parseInput "Day15/input.txt"
+                    |> Solution.part1
+    test <@ result = 508  @>
+
+[<Fact>]
+let ``augmentation test``()=
+    let m = parseInput "Day15/example.txt"
+    let r = Solution.augment m
+    test <@ r[0, *] =  [|1;1;6;3;7;5;1;7;4;2;2;2;7;4;8;6;2;8;5;3;3;3;8;5;9;7;3;9;6;4;4;4;9;6;1;8;4;1;7;5;5;5;1;7;2;9;5;2;8;6 |] @>
+    test <@ r[10, *] = [|2;2;7;4;8;6;2;8;5;3;3;3;8;5;9;7;3;9;6;4;4;4;9;6;1;8;4;1;7;5;5;5;1;7;2;9;5;2;8;6;6;6;2;8;3;1;6;3;9;7 |] @>
+    
+[<Fact>]   
+let ``example part2``()=
+       let result = parseInput "Day15/example.txt"
+                    |> Solution.part2
+       test <@ result = 315  @>
+
+[<Fact>]
+let ``part 2``() =
+       let result = parseInput "Day15/input.txt"
+                    |> Solution.part2
+       test <@ result = 2872  @>
