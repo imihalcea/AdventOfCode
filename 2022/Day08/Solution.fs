675c9b313c55a38c62b1f081218cfe04d86e9164
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2022-12-08 22:54:50 +0100

    Day08

--- 2022/Day08/Solution.fs
+++ 2022/Day08/Solution.fs
@@ -1,9 +1,57 @@
 module _2022.Day08.Solution
 
 open System
+open Microsoft.FSharp.Core
 
 
-let parseData (lines : string seq):int[,]=
-    lines |> Seq.map (fun line -> line.ToCharArray() |> Seq.map Convert.ToInt32)
+let parseInput (lines : string array):int[,]=
+    lines |> Array.map (fun line -> line |> Seq.map (fun c -> Convert.ToInt32(c.ToString())))
     |> array2D
 
+let collinearTrees  (t:int*int) (m:int[,])=
+    let r,c = t
+    [|m[r,..c-1] |> Array.rev ; m[r,c+1..]; m[..r-1,c] |> Array.rev; m[r+1..,c]|]
+
+let isVisible (m:int[,]) (t:int*int)=
+    let v = m[fst(t),snd(t)]
+    collinearTrees t m
+    |> Array.map (Array.forall (fun x -> x < v )) 
+    |> set |> Set.contains true
+
+let takeUntil (predicate:int -> bool) (s:int seq):int array =
+    let mutable hit = false
+    seq{
+        for x in s do
+            if (not hit) && predicate(x) then
+                hit <- true
+                yield x
+            else if not hit then
+                yield x     
+    } |> Array.ofSeq
+
+let computeScore (m:int[,]) (t:int*int)=
+    let v = m[fst(t),snd(t)]
+    collinearTrees t m
+    |> Array.map (takeUntil (fun x -> x >= v ))
+    |> Array.map Array.length
+    |> Array.fold (*) 1
+
+let points rows cols =
+    seq{
+        for r in 0..rows do
+            for c in 0..cols do
+                yield (r,c)
+    }
+
+let countVisible (m:int[,]) =
+    points (m.[*,0].Length - 1) (m.[0,*].Length - 1)
+    |> Seq.map (isVisible m)
+    |> Seq.filter id
+    |> Seq.length
+
+let bestScore (m:int[,]) =
+    points (m.[*,0].Length - 1) (m.[0,*].Length - 1)
+    |> Seq.map (computeScore m)
+    |> Seq.sortDescending
+    |> Seq.head
+    
--- 2022/Day08/Tests.fs
+++ 2022/Day08/Tests.fs
@@ -1,5 +1,61 @@
 module _2022.Day08.Tests
 
+open System.IO
 open Solution
 open Xunit
 open Swensen.Unquote
+
+[<Fact>]
+let ``collinear trees``()=
+    let result =
+        File.ReadAllLines("Day08/ex.txt")
+        |> parseInput
+        |> collinearTrees (2,2)
+    test <@ result = [| [|5;6|]; [|3;2|]; [|5;3|]; [|5;3|]  |] @>
+
+[<Fact>]
+let ``visibility``() =
+    let m =
+        File.ReadAllLines("Day08/ex.txt")
+        |> parseInput
+    test <@ isVisible m (1,1)  = true @>
+    test <@ isVisible m (1,3)  = false @>
+
+[<Fact>]
+let ``count visible``() =
+    let result =
+        File.ReadAllLines("Day08/ex.txt")
+        |> parseInput
+        |> countVisible 
+    test <@ result = 21 @>
+
+[<Fact>]
+let ``part1``() =
+    let result =
+        File.ReadAllLines("Day08/input.txt")
+        |> parseInput
+        |> countVisible 
+    test <@ result = 1713 @>
+
+[<Fact>]
+let ``take Until``()=
+    test <@ (takeUntil (fun x -> x>=3) [|6;5|]) = [|6|] @>
+
+
+[<Fact>]
+let ``compute scenic score``()=
+    let m =
+        File.ReadAllLines("Day08/ex.txt")
+        |> parseInput
+    test <@ computeScore m (1,2)  = 4 @>
+    test <@ computeScore m (3,2)  = 8 @>
+    test <@ computeScore m (2,2)  = 1 @>
+
+
+[<Fact>]
+let ``part2``() =
+    let result =
+        File.ReadAllLines("Day08/input.txt")
+        |> parseInput
+        |> bestScore 
+    test <@ result = 268464 @>
